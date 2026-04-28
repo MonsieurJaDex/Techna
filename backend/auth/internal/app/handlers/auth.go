2cdcc75877286c06153ff61968847fdd0c7eda83
MonsieurJaDex <coder.dinya@gmail.com>
2026-04-28 10:33:27 +0300

    Added renewing access token

--- backend/auth/internal/app/handlers/auth.go
+++ backend/auth/internal/app/handlers/auth.go
@@ -19,6 +19,72 @@ type AuthRoutes struct {
 	Svc *service.TokenSvc
 }
 
+func (r *AuthRoutes) GetNewAccessToken(c *gin.Context) {
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Reading body error",
+		})
+		return
+	}
+
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(c.Request.Body)
+
+	if len(body) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Empty body",
+		})
+		return
+	}
+
+	var object dto.RefreshDto
+
+	err = json.Unmarshal(body, &object)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Failed to unmarshal JSON",
+		})
+		return
+	}
+
+	// validate body
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	err = validate.Struct(object)
+
+	var errs validator.ValidationErrors
+	errors.As(err, &errs)
+
+	errorString := ""
+
+	if len(errs) > 0 {
+		for _, err := range errs {
+			errorString += fmt.Sprintf("%s: %s\r", err.Field(), err.Error())
+		}
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": errorString,
+		})
+		return
+	}
+
+	newAccess, newRefresh, err := r.Svc.RenewAccessToken(object.RefreshToken)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, struct {
+		AccessToken  string `json:"access_token"`
+		RefreshToken string `json:"refresh_token"`
+	}{
+		AccessToken:  newAccess,
+		RefreshToken: newRefresh,
+	})
+}
+
 func (r *AuthRoutes) SignIn(c *gin.Context) {
 	body, err := io.ReadAll(c.Request.Body)
 	if err != nil {
